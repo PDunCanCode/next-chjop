f28d3a397d54b67d1774cfb1d47e413fbe87aefe
Paul D. Duncan <chesiredarling@gmail.com>
2020-04-05 19:16:46 -0700

    Aint nobody got time for Covid

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MainLayout } from '../components/layout/MainLayout';
+
+const StyledHeader = styled.h1`
+  ${({ theme }) => `
+  font-size: 2em;
+  text-align: left;
+  padding: ${theme['padding-small']} ${theme['padding-small']};
+  `}
+`;
+
+const Index = () => {
+  return (
+    <MainLayout>
+      <StyledHeader>Index Page</StyledHeader>
+    </MainLayout>
+  );
+};
+
+export default Index;
