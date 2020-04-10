7f726d4c00216462bdbd26ce91a753c1b5870814
Paul D. Duncan <chesiredarling@gmail.com>
2020-04-09 22:05:40 -0700

    Added submitForm && Dropdown

--- utils/submitForm.ts
+++ utils/submitForm.ts
@@ -8,9 +8,27 @@ export const submitForm = (initialValues, callback) => {
     callback();
     setInputs(() => ({ ...initialValues }));
   };
+  const handleInputChange = event => {
+    event.persist();
+    setInputs(inputs => {
+      const newInputs = _.cloneDeep(inputs);
+      _.set(newInputs, event.target.name, event.target.value);
+      return newInputs;
+    });
+  };
+
+  const handleDropdownChange = event => {
+    setInputs(inputs => {
+      const newInputs = _.cloneDeep(inputs);
+      _.set(newInputs, event.item.props.title, event.key);
+      return newInputs;
+    });
+  };
 
   return {
     inputs,
     handleSubmit,
+    handleDropdownChange,
+    handleInputChange
   };
 };
